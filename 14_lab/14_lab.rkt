56deec7e1480b78b7e28421eafe6dbe9cfe5e994
Anton Vlasov <anton.vlasov@jetstyle.ru>
2020-02-25 21:46:30 +0500

    sorted 14_lab

--- 14_lab/14_lab.rkt
+++ 14_lab/14_lab.rkt
@@ -1,79 +1,6 @@
 #lang racket
 
 
-; 4
-
-(define (leaf? subtree)
-  (andmap empty? (cdr subtree))
-  )
-
-(define (count_leaves tree)
-  (cond
-    [(empty? tree) 0]
-    [(leaf? tree) 1]
-    [else
-     (+
-      (count_leaves (cadr tree))
-      (count_leaves (caddr tree))
-      )
-     ]
-    )
-  )
-
-; 3
-
-(define (max_in_tree tree)
-  (cond
-    [(empty? tree) 0]
-    [(leaf? tree) (car tree)]
-    [else
-     (max
-      (car tree)
-      (max_in_tree (cadr tree))
-      (max_in_tree (caddr tree))
-      )
-     ]
-    )
-  )
-
-
-(define (get_counter node max_node)
-  (if (= (car node) max_node)
-      1
-      0
-      )
-  )
-
-(define (count_maxes tree)
-  (define max_node (max_in_tree tree))
-  (define (count_recursion tree)
-    (cond
-      [(empty? tree) 0]
-      [else
-       (let ([counter (get_counter tree max_node)])
-        (+
-         counter
-         (count_recursion (cadr tree))
-         (count_recursion (caddr tree))
-         )
-        )
-       ]
-      )
-    )
-  (count_recursion tree)
-  )
-
-
-; 5
-
-(define (binary? tree)
-  (if (or (empty? tree) (leaf? tree))
-      #t
-      (and (= (length tree) 3) (binary? (cadr tree)) (binary? (caddr tree)))
-      )
-  )
-
-
 ; 1
 
 (define (get_triplets N_list)
@@ -134,4 +61,78 @@
     )
 
   (get_starting_point directories)
+  )
+
+
+; 3
+
+(define (max_in_tree tree)
+  (cond
+    [(empty? tree) 0]
+    [(leaf? tree) (car tree)]
+    [else
+     (max
+      (car tree)
+      (max_in_tree (cadr tree))
+      (max_in_tree (caddr tree))
+      )
+     ]
+    )
+  )
+
+
+(define (get_counter node max_node)
+  (if (= (car node) max_node)
+      1
+      0
+      )
+  )
+
+(define (count_maxes tree)
+  (define max_node (max_in_tree tree))
+  (define (count_recursion tree)
+    (cond
+      [(empty? tree) 0]
+      [else
+       (let ([counter (get_counter tree max_node)])
+        (+
+         counter
+         (count_recursion (cadr tree))
+         (count_recursion (caddr tree))
+         )
+        )
+       ]
+      )
+    )
+  (count_recursion tree)
+  )
+
+
+; 4
+
+(define (leaf? subtree)
+  (andmap empty? (cdr subtree))
+  )
+
+(define (count_leaves tree)
+  (cond
+    [(empty? tree) 0]
+    [(leaf? tree) 1]
+    [else
+     (+
+      (count_leaves (cadr tree))
+      (count_leaves (caddr tree))
+      )
+     ]
+    )
+  )
+
+
+; 5
+
+(define (binary? tree)
+  (if (or (empty? tree) (leaf? tree))
+      #t
+      (and (= (length tree) 3) (binary? (cadr tree)) (binary? (caddr tree)))
+      )
   )
