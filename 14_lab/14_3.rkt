3e2ca3ddf8a5d7eb65f6c79b7913a17218daa941
Anton Vlasov <anton.vlasov@jetstyle.ru>
2020-02-29 23:15:59 +0500

    переписал подсчёт максимума в один проход по дереву

---
+++ 14_lab/14_3.rkt
@@ -0,0 +1,67 @@
+#lang racket
+
+
+(define (leaf? subtree)
+  (andmap empty? (cdr subtree))
+  )
+
+
+(define (left tree)
+  (cadr tree)
+  )
+
+(define (right tree)
+  (caddr tree)
+  )
+
+(define (get_max_pair pair1 pair2 current_max current_counter)  ; TODO: do the logic
+  (define max1 (car pair1))
+  (define max2 (car pair2))
+  (cond
+    [(= max1 max2)
+     (if (= max1 current_max)
+         (cons max1 (- (+ (cdr pair1) (cdr pair2)) current_counter))
+         (cons max1 (+ (cdr pair1) (cdr pair2)))
+         )
+     ]
+    [(> max1 max2) pair1]
+    [else pair2]
+    )
+  )
+
+
+(define (count_maxes tree)
+  (define (rec tree current_max counter)
+    (cond
+      [(empty? tree) (cons current_max counter)]
+      [(leaf? tree)
+       (cond
+         [(= current_max (car tree)) (cons current_max (+ counter 1))]
+         [(< current_max (car tree)) (cons (car tree) 1)]
+         [else (cons current_max counter)]
+         )
+       ]
+      [else
+       (let* ([node (car tree)]
+              [curr_max_data
+               (cond
+                 [(= node current_max) (cons current_max (+ counter 1))]
+                 [(> node current_max) (cons node 1)]
+                 [else (cons current_max counter)]
+                 )
+               ]
+              [new_current_max (car curr_max_data)]
+              [new_counter (cdr curr_max_data)]
+              )
+         (get_max_pair
+          (rec (left tree) new_current_max new_counter)
+          (rec (right tree) new_current_max new_counter)
+          new_current_max
+          new_counter
+          )
+         )
+       ]
+      )
+   )
+  (cdr (rec tree -inf.0 0))
+  )
