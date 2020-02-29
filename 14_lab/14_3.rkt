07b04fa323a994f113cd152e6ddf8f4d2cb7b07a
Anton Vlasov <anton.vlasov@jetstyle.ru>
2020-02-29 23:19:55 +0500

    refactor task_3

--- 14_lab/14_3.rkt
+++ 14_lab/14_3.rkt
@@ -30,26 +30,23 @@
   )
 
 
+(define (get_max_data tree current_max counter)
+  (define node (car tree))
+  (cond
+    [(= current_max node) (cons current_max (+ counter 1))]
+    [(< current_max node) (cons node 1)]
+    [else (cons current_max counter)]
+    )
+  )
+
 (define (count_maxes tree)
   (define (rec tree current_max counter)
     (cond
       [(empty? tree) (cons current_max counter)]
-      [(leaf? tree)
-       (cond
-         [(= current_max (car tree)) (cons current_max (+ counter 1))]
-         [(< current_max (car tree)) (cons (car tree) 1)]
-         [else (cons current_max counter)]
-         )
-       ]
+      [(leaf? tree) (get_max_data tree current_max counter)]
       [else
        (let* ([node (car tree)]
-              [curr_max_data
-               (cond
-                 [(= node current_max) (cons current_max (+ counter 1))]
-                 [(> node current_max) (cons node 1)]
-                 [else (cons current_max counter)]
-                 )
-               ]
+              [curr_max_data (get_max_data tree current_max counter)]
               [new_current_max (car curr_max_data)]
               [new_counter (cdr curr_max_data)]
               )
